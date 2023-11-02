0a218e51cdc2e06c7fef7d2acccf1da1cb258273
lauren.benezra@dbtlabs.com <lauren.benezra@dbtlabs.com>
2023-11-02 01:52:54 +0000

    add audit helper and unit test

--- models/staging/tpch/stg_tpch__customers.sql
+++ models/staging/tpch/stg_tpch__customers.sql
@@ -5,7 +5,7 @@
 }}
 with source as (
 
-    select * from {{ source('tpch', 'customer') }}
+    select * from {{ ref('pre_stg_tpch__customers') }}
 
 ),
 
@@ -13,16 +13,16 @@ renamed as (
 
     select
         -- ids
-        c_custkey, -- as customer_id,
-        c_nationkey,-- as nation_id,
+        c_custkey as customer_id,
+        c_nationkey as nation_id,
 
         -- descriptions
-        c_name,-- as name,
-        c_address,-- as address,
-        c_phone,-- as phone_number,
-        c_acctbal,-- as account_balance,
-        c_mktsegment,-- as market_segment,
-        c_comment-- as comment
+        c_name as name,
+        c_address as address,
+        c_phone as phone_number,
+        c_acctbal as account_balance,
+        c_mktsegment as market_segment,
+        c_comment as comment
 
     from source
 
